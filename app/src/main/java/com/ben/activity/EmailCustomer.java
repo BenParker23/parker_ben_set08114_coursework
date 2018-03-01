19710a780babecf5551dfe6472f4dc91bd71e529
BenParker23 <benparker_93@outlook.com>
2018-03-01 18:09:04 +0000

    Adding email functionality and activity

--- app/src/main/java/com/ben/activity/EmailCustomer.java
+++ app/src/main/java/com/ben/activity/EmailCustomer.java
@@ -1,7 +1,12 @@
 package com.ben.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.ben.R;
 
@@ -9,11 +14,35 @@ import com.ben.R;
  * Created by ben on 17/02/18.
  */
 
-public class EmailCustomer extends AppCompatActivity {
+public class EmailCustomer extends AppCompatActivity implements View.OnClickListener{
+
+
+    private Button sendEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emailcustomer);
+        /** Hide the keyboard on load of activity **/
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        sendEmail = (Button)findViewById(R.id.sendEmailBut);
+        sendEmail.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        sendEmail();
+    }
+
+    private void sendEmail(){
+
+        Intent intent = new Intent(Intent.ACTION_SEND); // it's not ACTION_SEND
+        intent.setType("message/rfc822");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Subject of email");
+        intent.putExtra(Intent.EXTRA_TEXT, "Body of email");
+        intent.setData(Uri.parse("mailto:benparker_93@outlook.com")); // or just "mailto:" for blank
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // this will make such that when user returns to your app, your app is displayed, instead of the email app.
+        startActivity(intent);
     }
 }
